7321d882ba1fe31db2a811ba99c3a9cf43043197
dustin-ww <wickertdustin@gmail.com>
2025-11-16 16:22:28 +0100

    fix watermark error & dependency versions

--- app/spark_stream_pg.py
+++ app/spark_stream_pg.py
@@ -1,31 +1,37 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import avg, from_json, col, current_timestamp
 from pyspark.sql.types import StructType, StructField, StringType, DoubleType, IntegerType
-import time
 
 spark = SparkSession.builder \
     .appName("CityMoodToPostgres") \
     .master("spark://spark-master:7077") \
-    .config("spark.jars.packages", "org.postgresql:postgresql:42.7.2,org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0") \
+    .config("spark.jars.packages", "org.postgresql:postgresql:42.7.8,org.apache.spark:spark-sql-kafka-0-10_2.13:4.0.1") \
+    .config("spark.sql.streaming.statefulOperator.checkCorrectness.enabled", "false") \
     .getOrCreate()
 
 kafka_schema = StructType([
     StructField("fetch_timestamp", StringType(), True),
     StructField("source", StringType(), True),
     StructField("feature_index", IntegerType(), True),
-    StructField("feature", StringType(), True)  
+    StructField("feature", StringType(), True)
 ])
+
 
 df = spark.readStream \
     .format("kafka") \
     .option("kafka.bootstrap.servers", "kafka:9092") \
-    .option("subscribe", "api-data") \
+    .option("subscribe", "hh-traffic-data") \
     .option("startingOffsets", "latest") \
-    .load()
+    .load() \
+    .drop("timestamp")
+
+
 
 parsed_df = df.select(
     from_json(col("value").cast("string"), kafka_schema).alias("data")
-).select("data.*")
+).select("data.*") \
+ .withColumnRenamed("fetch_timestamp", "event_meta_timestamp")   
+
 
 scored_df = parsed_df \
     .groupBy("source") \
@@ -33,16 +39,19 @@ scored_df = parsed_df \
     .withColumnRenamed("count", "feature_count") \
     .withColumn("score", col("feature_count").cast(DoubleType()))
 
+
 overall_score = scored_df.agg(
     avg("score").alias("avg_score")
 )
 
+
+# --- WRITE TO POSTGRES ---
 def write_to_postgres(batch_df, batch_id):
     if not batch_df.isEmpty():
         (
             batch_df
             .withColumn("updated_at", current_timestamp())
-            .withColumn("metric_name", col("avg_score"))  
+            .withColumn("metric_name", col("avg_score"))
             .write
             .format("jdbc")
             .option("url", "jdbc:postgresql://postgres:5432/city_mood")
@@ -54,6 +63,7 @@ def write_to_postgres(batch_df, batch_id):
             .save()
         )
         print(f"Batch {batch_id} geschrieben")
+
 
 query = (
     overall_score.writeStream

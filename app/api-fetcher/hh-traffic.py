dbe6000fcafeac6d4de13be712b159101a42a8e9
dustin-ww <wickertdustin@gmail.com>
2025-11-19 23:08:24 +0100

    rework doc

--- app/api-fetcher/hh-traffic.py
+++ app/api-fetcher/hh-traffic.py
@@ -13,7 +13,7 @@ from pathlib import Path
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-KAFKA_BOOTSTRAP_SERVERS = os.getenv('KAFKA_BOOTSTRAP_SERVERS', 'localhost:29092')
+KAFKA_BOOTSTRAP_SERVERS = os.getenv('KAFKA_BOOTSTRAP_SERVERS', 'kafka:9092')
 KAFKA_TOPIC = 'hh-traffic-data'
 INPUT_DIR = './../../data/traffic_hh'
 

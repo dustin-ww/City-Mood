3edcb02a2facf0ab43afccba10a83805054c6b73
dustin-ww <wickertdustin@gmail.com>
2026-01-02 20:53:18 +0100

    feat: rework container building

--- deploy-system.sh
+++ deploy-system.sh
@@ -2,18 +2,18 @@
 set -e
 
 echo "Starting kafka, spark instances, postgres and grafana..."
-docker-compose up -d 
+#docker compose up -d 
 
 echo "Starting to build api fetching services in dedicated containers..."
 
 # Config
 BASE_IMAGE="city-mood-python-base"
-BASE_TAG="1.0.0"
+BASE_TAG="latest"
 
 echo "Building base image..."
 docker build \
   -t ${BASE_IMAGE}:${BASE_TAG} \
-  -f /app/services/Dockerfile .
+  -f ./app/Dockerfile .
 
 SERVICES=(
   scheduler
@@ -23,8 +23,8 @@ SERVICES=(
 for SERVICE in "${SERVICES[@]}"; do
   echo "🔨 Building ${SERVICE}..."
   docker build \
-    -t ${SERVICE}:latest \
-    -f /app/services/${SERVICE}/Dockerfile .
+    -t city-mood-${SERVICE}:latest \
+    -f ./app/services/${SERVICE}/Dockerfile .
 done
 
 echo "All images built!"
